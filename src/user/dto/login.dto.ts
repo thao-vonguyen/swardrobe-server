9778c9cdd57b2c725ed61ea715b1d21e3c597936
dotiennee <149573030+dotiennee@users.noreply.github.com>
2025-05-31 22:58:59 +0700

    Api responseresponse

--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { UserResponseDto } from './user.dto';
 
 export class LoginDto {
     @ApiProperty({ example: 'user@example.com' })
@@ -6,4 +7,17 @@ export class LoginDto {
 
     @ApiProperty({ example: 'password123' })
     password: string;
+}
+
+export class LoginResponseDto {
+    @ApiProperty({ example: 'Login successful' })
+    message: string;
+
+    @ApiProperty({
+        example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+    })
+    access_token: string;
+
+    @ApiProperty({ type: UserResponseDto })
+    user: UserResponseDto;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserDto } from './dto/user.dto';
-import { LoginDto } from './dto/login.dto';
+import { UserDto, UserGetResponseDto, UserResponseDto } from './dto/user.dto';
+import { LoginDto, LoginResponseDto } from './dto/login.dto';
 import { Public } from 'src/auth/public.decorator';
-import { ApiBearerAuth, ApiBody } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse } from '@nestjs/swagger';
 @ApiBearerAuth()
 @Controller('users')
 export class UserController {
@@ -12,6 +12,10 @@ export class UserController {
   @Public() // Đánh dấu route này là công khai, không cần xác thực
   @Post('register')
   @ApiBody({ type: UserDto })
+  @ApiCreatedResponse({
+    description: 'Tạo người dùng thành công',
+    type: UserResponseDto,
+  })
   create(@Body() body: UserDto) {
     return this.userService.create(body);
   }
@@ -19,20 +23,28 @@ export class UserController {
   @Public() // Đánh dấu route này là công khai, không cần xác thực
   @Post('login')
   @ApiBody({ type: LoginDto })
+  @ApiCreatedResponse({
+    description: 'Đăng nhập thành công',
+    type: LoginResponseDto,
+  })
   async login(@Body() body: LoginDto) {
     return this.userService.login(body.email, body.password);
   }
 
   @Patch(':id') // Sử dụng PATCH để cập nhật dữ liệu
+  @ApiOkResponse({ description: 'Cập nhật người dùng thành công' })
   update(@Param('id') id: string, @Body() body: UserDto) {
     return this.userService.update(+id, body); // gọi phương thức update trong UserService
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: UserGetResponseDto, description: 'Thông tin người dùng' })
+  @ApiNotFoundResponse({ description: 'Không tìm thấy người dùng' })
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @ApiOkResponse({ schema: { example: { message: 'Xóa người dùng thành công' } } })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
